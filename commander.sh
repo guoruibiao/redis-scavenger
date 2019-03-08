54838757ef11ff77bc4641f3f7ea628080a837e1
guoruibiao <marksinoberg@gmail.com>
2019-03-08 11:55:03 +0800

    add filter parameters
    添加对通用key前缀的查找

--- commander.sh
+++ commander.sh
@@ -5,6 +5,10 @@ KEYS_FILE=$1
 WITHOUT_TTL_FILE=$2
 echo $KEYS_FILE
 echo $WITHOUT_TTL_FILE
-return 0
+
+# 拿到没有过期时间的key列表
 paste -d " " $KEYS_FILE $WITHOUT_TTL_FILE | grep -o ".*-1" | cut -d " " -f 1 > without_ttl
 echo "keys without ttl has beed saved in without_ttl"
+
+# 对without_ttl 文件查找具备频繁使用最通用前缀（由此看出，代码规范中对key的规范是多么重要，比如使用下划线_ 或者冒号: 进行分隔）
+cat scankeys.txt | sort | cut -d ":" -f 1 | uniq -c | sort -nr | head  
